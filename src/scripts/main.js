e0c6d290d83ca6367b85debe3ce0b822ca9eb830
itspoma <itspoma@gmail.com>
2017-12-16 18:28:47 +0200

    added create-pull action

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,8 @@ const app = angular.module('votestApp', []);
 app.controller('pageController', ['$injector', function($injector) {
   var vm = this;
   vm.message = 'Hello world';
+
+  vm.createPoll = () => {
+    alert(1);
+  }
 }]);
